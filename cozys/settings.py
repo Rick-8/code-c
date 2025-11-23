72dc574f1e239b841fcb5b713ecd0ef64dfc3290
Rick-8 <npopbwls@students.codeinstitute.net>
2025-11-23 21:43:24 +0000

    Update - Settings

--- cozys/settings.py
+++ cozys/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # WARNING: set DEBUG = False in production
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ["*"]
 
 
 # -------------------------------------------------------------------
