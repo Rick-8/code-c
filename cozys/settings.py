8c06cef4a116fcfcb82f595b2aba4845dbe01fbc
Rick-8 <npopbwls@students.codeinstitute.net>
2025-11-23 21:34:01 +0000

    Update - Settings

--- cozys/settings.py
+++ cozys/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # WARNING: set DEBUG = False in production
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
