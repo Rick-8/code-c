d61fced6d1aa5775f35ee390c51bc2b1203f70fd
Rick-8 <npopbwls@students.codeinstitute.net>
2025-11-30 22:09:28 +0000

    Update shop management code

--- shop/context_processors.py
+++ shop/context_processors.py
@@ -1,5 +1,9 @@
+from .models import ShopSettings
+
+
 def shop_settings(request):
-    ordering = request.session.get("ordering_enabled", True)
+    settings_obj, created = ShopSettings.objects.get_or_create(id=1)
     return {
-        "ordering_enabled": ordering
+        "ordering_enabled": settings_obj.ordering_enabled,
+        "shop_open": settings_obj.is_shop_open,
     }
